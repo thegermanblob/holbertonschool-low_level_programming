79036ed6f36c6f049ac6f650eaa8ec0067cd58f4
thegermanblob <thegermanblob@gmail.com>
2021-02-03 19:53:05 +0000

    Add quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,6 +9,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 0;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 64 );
+	return (1);
 }
