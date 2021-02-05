b5d9ce9e16c998a9fb05257dad0f8b40cf00d3b6
thegermanblob <thegermanblob@gmail.com>
2021-02-05 00:24:02 +0000

    Add print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int c;
+	int ci; /*count decimas*/
+
+	c = 48;
+	for (ci = 48; (ci < 58); ci++)
+	{
+	for (c = 48; c < 58; c++)
+	{
+	if (c > ci)
+	{
+	putchar(ci);
+	putchar(c);
+	if ((ci != 57) || (c != 57))
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
