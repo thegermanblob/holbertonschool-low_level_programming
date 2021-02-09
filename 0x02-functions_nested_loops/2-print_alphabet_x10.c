4d66a3889a794c6bf1d2823021dddbbfb237dc6c
Thegermanblob <thegermanblob@gmail.com>
2021-02-09 13:01:14 +0000

    Add print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
