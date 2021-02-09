309d963cb337578b477adac1d44af5e45892ee52
Thegermanblob <thegermanblob@gmail.com>
2021-02-09 14:19:33 +0000

    Make print to 98 betty compliant

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,37 +1,38 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
+ * print_to_98 - prints to number 98
+ * @n: starting num
  *
  */
 void print_to_98(int n)
 {
 	int c;
+
 	c = n;
-	if(c > 98)
+	if (c > 98)
 	{
-		for(;c > 97;c--)
+		for (; c > 97; c--)
 		{
 			printf("%i", c);
-			if(c != 98)
+			if (c != 98)
 				printf(", ");
-			if(c == 98)
+			if (c == 98)
 				printf("\n");
 		}
 	}
 	else if (c < 98)
 	{
-		for(; c < 99; c++)
+		for (; c < 99; c++)
 		{
 			printf("%i", c);
-			if(c != 98)
+			if (c != 98)
 				printf(", ");
 			else
 				printf("\n");
 		}
 	}
 	else
-		printf("%i\n",n);
+		printf("%i\n", n);
 }
 
