7242fe678876cd9b01cdff260a6e9165c7a87e72
Thegermanblob <thegermanblob@gmail.com>
2021-02-09 13:05:16 +0000

    Add 10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * add -adds given numbers
+ * @a: number 1 to add
+ * @b: number 2 to add
+ *
+ * Return: returns result of addition
+ */
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+	return (x);
+}
