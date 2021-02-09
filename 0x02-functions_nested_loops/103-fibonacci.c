5c0776589747e1132d1364b81168bdedba3783f3
Thegermanblob <thegermanblob@gmail.com>
2021-02-09 17:32:32 +0000

    Add fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print fibonacci
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int i, x, y, r, rr;
+
+	x = 0;
+	y = 1;
+	for (i = 0; i < 33; i++)
+	{
+		r = x + y;
+		x = y;
+		y = r;
+		if ((r % 2) == 0)
+			rr = rr + r;
+	}
+	printf("%lu\n", rr);
+	return (0);
+}
