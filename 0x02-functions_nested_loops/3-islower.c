99b32602a30a59550f4a8bcfa80247e198e745a4
Thegermanblob <thegermanblob@gmail.com>
2021-02-09 13:32:42 +0000

    Fix islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * _islower returns 1 if it is a lower case letter
+ * _islower - returns 1 if it is a lower case letter
  *
- * int c is the numeric value of char to be tested
+ * @c: is the numeric value of char to be tested
  *
  * Return: 1 is a lower case
  */
