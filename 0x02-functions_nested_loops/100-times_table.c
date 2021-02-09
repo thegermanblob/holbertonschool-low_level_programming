f1ff101e322c12df9eab8014cd1e9c573b9d3e0a
Thegermanblob <thegermanblob@gmail.com>
2021-02-09 15:08:29 +0000

    Add times table, update holberton

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * times_table - print muliplication tables 0 - 9
+ *
+ *
+ */
+void print_times_table(int n)
+{
+	int x, y, r;
+
+	if(n >=0 && n<= 15)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			for (x = 0; x <= n; x++)
+			{
+				r = y * x;
+				
+				if (x != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (r < 10)
+					_putchar(' ');
+				if (r < 100)
+					_putchar(' ');
+				if ((r >= 100) && (r > 10))
+				{
+					_putchar((r / 100) + '0');
+					_putchar(((r / 10) % 10) + '0');
+				}
+				else if (r >= 10)
+					_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
