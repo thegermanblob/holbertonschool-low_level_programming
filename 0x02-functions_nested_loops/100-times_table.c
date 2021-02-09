bc5e6abc4dec5df7664037ad504d8897f3d687f6
Thegermanblob <thegermanblob@gmail.com>
2021-02-09 15:14:48 +0000

    Make times betty compliant

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,29 +1,28 @@
 #include "holberton.h"
 /**
- * times_table - print muliplication tables 0 - 9
- *
+ * print_times_table - print muliplication tables 0 - 9
+ * @n: number for table
  *
  */
 void print_times_table(int n)
 {
 	int x, y, r;
 
-	if(n >=0 && n<= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (y = 0; y <= n; y++)
 		{
 			for (x = 0; x <= n; x++)
 			{
 				r = y * x;
-				
 				if (x != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (r < 10)
+				if ((r < 10) && (x != 0))
 					_putchar(' ');
-				if (r < 100)
+				if ((r < 100) && (x != 0))
 					_putchar(' ');
 				if ((r >= 100) && (r > 10))
 				{
