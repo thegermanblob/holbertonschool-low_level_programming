92dc929191ce164462f39768def9621e4e210466
Thegermanblob <thegermanblob@gmail.com>
2021-02-10 03:56:39 +0000

    Add 104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,43 @@
  */
 int main(void)
 {
-	float i, x, y, r;
+	unsigned long i, x, y, r;
+	unsigned long x1, x2, y1, y2;
+	unsigned long r1, r2;
 
-	x = 1;
-	y = 2;
-	printf("1, 2, ");
-	for (i = 0; i < 96; i++)
+	x = 0;
+	y = 1;
+
+	for (i = 0; i < 92; i++)
 	{
 		r = x + y;
 		x = y;
 		y = r;
-		/*printf("%li, ", x);*/
-		printf("%.0f", r);
-		if (i == 95)
-			printf("\n");
-		else
-			printf(", ");
+		printf("%lu, ", r);
 	}
+	x1 = x / 10000000000;
+	x2 = x % 10000000000;
+	y1 = y / 10000000000;
+	y2 = y / 10000000000;
+
+	for (i = 93; i < 99; i++)
+	{
+		r1 = x1 + y1;
+		r2 = x2 + y2;
+		if (x2 + y2 > 9999999999)
+		{
+			r1 += 1;
+			r2 %= 1000000000;
+		}
+		printf("%lu%lu", r1, r2);
+		if (i != 98)
+			printf(", ");
+		x1 = y1;
+		x2 = y2;
+		y1 = r1;
+		y2 = r2;
+	}
+	printf("\n");
+
 	return (0);
 }
