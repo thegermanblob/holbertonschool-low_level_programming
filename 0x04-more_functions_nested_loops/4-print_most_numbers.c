02f3d4eba5a6e942c372816b3fa86b61c3b2a1f5
Thegermanblob <thegermanblob@gmail.com>
2021-02-11 18:41:30 +0000

    Fix print most num

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,14 @@
  * Return: n/a
  *
  */
-int print_most_numbers(void)
+void print_most_numbers(void)
 {
 	int n;
 
 	for (n = 48; n < 58; n++)
 	{
-		if ((n != 2) && (n != 4))
+		if ((n != 50) && (n != 52))
 			_putchar(n);
 	}
 	_putchar('\n');
-	return (0);
 }
