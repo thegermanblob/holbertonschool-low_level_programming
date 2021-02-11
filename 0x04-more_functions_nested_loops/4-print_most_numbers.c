9e10f98df2dbc5af3be47c0df2137416641acb7e
Thegermanblob <thegermanblob@gmail.com>
2021-02-11 18:38:55 +0000

    Fix print most num

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 
 /**
- * print_numbers- prints 0 to 9
+ * print_most_numbers- prints 0 to 9 skips 2 & 4
  * Return: n/a
  *
  */
-int print_numbers(void)
+int print_most_numbers(void)
 {
 	int n;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n < 58; n++)
 	{
 		if ((n != 2) && (n != 4))
 			_putchar(n);
 	}
 	_putchar('\n');
+	return (0);
 }
