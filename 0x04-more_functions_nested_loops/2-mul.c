fb5a197fbeb18e31fb6cadfebd38a5fc42ee973b
Thegermanblob <thegermanblob@gmail.com>
2021-02-11 18:30:01 +0000

    Make mul betty compliant

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
- *
+ * mul - multiplies
+ * @x: num 1
+ * @y: num 2
+ * Return: product
  *
  */
 int mul(int x, int y)
 {
 	int z;
-	z= x * y;
-	return(z);
+
+	z = x * y;
+	return (z);
 }
