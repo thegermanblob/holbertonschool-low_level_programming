e0728fbc00e460cda3dd85373c36d1bcb00f0d70
Thegermanblob <thegermanblob@gmail.com>
2021-02-11 18:17:06 +0000

    Update holberton

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,9 @@
+int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
+int print_numbers(void);
+void print_most_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_space(int);
