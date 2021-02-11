c263809a860ebc6f7864709f2aa34c641dd91c8e
Thegermanblob <thegermanblob@gmail.com>
2021-02-11 18:26:57 +0000

    Make isupper betty compliant

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _isupper - checks if uppercase
+ * @n: char to be checked
+ * Return: void
  */
 int _isupper(int n)
 {
-	if ((n >=65) && (n <= 91))
+	if ((n >= 65) && (n <= 90))
 		return (1);
 	else
 		return (0);
