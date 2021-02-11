fad041238ea0e5dc35f7208e7aef714331c3623c
Thegermanblob <thegermanblob@gmail.com>
2021-02-11 19:02:47 +0000

    Fix print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,11 +12,12 @@ void print_line(int n)
 {
 	int i;
 
-	if(n == 0)
-		_putchar('\n');
-	for (i = 0; i <= n; i++)
+	if(n > 0)
 	{
-		_putchar('_');
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
