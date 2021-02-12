bce69965bbf8c323392548e2ba422818c2070cb7
Thegermanblob <thegermanblob@gmail.com>
2021-02-12 03:27:57 +0000

    Fix print

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@ void print_number(int n)
 {
 	if (n < 0)
 		n = -n;
-	if (ni / 10 != 0)
-		print_number(ni / 10);
-	_putchar(ni % 10 + '0');
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
