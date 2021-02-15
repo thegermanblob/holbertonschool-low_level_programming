71a4ec8fa530592f654795c0d8299620f1fbea03
Thegermanblob <thegermanblob@gmail.com>
2021-02-15 22:22:06 +0000

    Fix logic atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-void num_find(char *s, int *end, int *begin);
+int num_find(char *s, int *end, int *begin);
 int power(int base, int exp);
 /**
  *
@@ -9,31 +9,36 @@ int power(int base, int exp);
  */
 int _atoi(char *s)
 {
-	int result, end, begin, d, cn2, n, rr, sigh;
+	int result, end, begin, d, cn2, n, rr, sigh, p;
 	
 
 	result = 0;
 	sigh = 1;
-	num_find(s, &end, &begin);
+	begin = 0;
+	p = num_find(s, &end, &begin);
 	d = end - begin;
 	cn2 = begin;
-	
-	while (cn2 <= end)
+	if (p != 0)
 	{
-		if (s[cn2] == '-')
+		while (cn2 <= end)
 		{
-			sigh = sigh * (-1);
+			if (s[cn2] == '-')
+			{
+				sigh = sigh * (-1);
+			}
+			else if ((s[cn2] >= '0') && (s[cn2] <= '9'))
+			{
+				n = (s[cn2] - '0');
+				result = result + (power(10,d) * n);
+			}
+			cn2++;
+			d--;
 		}
-		else if ((s[cn2] >= '0') && (s[cn2] <= '9'))
-		{
-			n = (s[cn2] - '0');
-			result = result + (power(10,d) * n);
-		}
-		cn2++;
-		d--;
+		rr = (sigh * result);
+		return (rr);
 	}
-	rr = (sigh * result);
-	return (rr);
+	else
+		return (0);
 }
 /**
  *
@@ -42,26 +47,31 @@ int _atoi(char *s)
  *
  *
  */
-void num_find(char *s, int *end, int *begin)
+int num_find(char *s, int *end, int *begin)
 {
-	int count;
+	int count, p;
 
 	count = 0;
+	p = 0;
 	while (s[count] != '\0')
 	{
-		if (((s[count] >= '0') && (s[count] <= '9')) &&
-			((s[count + 1] >= '0') && (s[count + 1] >= '9')))
+		if ((((s[count] >= '0') && (s[count] <= '9')) &&
+			((s[count + 1] >= '0') && (s[count + 1] <= '9'))) && (*begin == 0))
 		{
-			*begin = count;
+			*begin = count - 1;
+			p++;
+
 		}
-		if (((s[count] >= '0') && (s[count] <= '9')) &&
+		else if (((s[count] >= '0') && (s[count] <= '9')) &&
 			((s[count + 1] < '0') || (s[count + 1] > '9') || (s[count + 1] == '\0')))
 		{
 			*end = count;
+			p++;
 			break;
 		}
 		count++;
 	}
+	return(p);
 }
 
 int power(int base, int exp) 
