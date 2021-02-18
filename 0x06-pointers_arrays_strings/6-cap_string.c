3a79d40f6c4ac1cd959d6a329e8dacc88949e185
Thegermanblob <thegermanblob@gmail.com>
2021-02-18 22:38:05 +0000

    Add cap_string, Append hol

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * cap_string - captions string
+ * @s: string to be
+ * Return: returns string
+ *
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		switch (s[i])
+		{
+			case 59:
+			case 44:
+			case '.':
+			case 33:
+			case 63:
+			case 34:
+			case 40:
+			case 41:
+			case 123:
+			case 32:
+			case 9:
+			case 10:
+				if ((s[i + 1] >= 'a') && (s[i + 1] <= 'z'))
+					s[i + 1] = s[i + 1] - 32;
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+	return (s);
+}
