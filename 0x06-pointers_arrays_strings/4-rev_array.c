38c7e9a17403eb46f0f14fcee9bc3b64eda58c99
Thegermanblob <thegermanblob@gmail.com>
2021-02-18 18:30:21 +0000

    Add rev array,Append holberton

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * reverse_array - as the name says
+ * @a: array to be reversed
+ * @n: size of array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, count;
+	int storage[500];
+
+	count = 0;
+	for (i = n - 1; i >= n; i--)
+	{
+		storage[count] = a[i];
+		count++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = storage[i];
+	}
+}
