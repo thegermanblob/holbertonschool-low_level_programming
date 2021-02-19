46a3d06e1e84fe45030031f4d4a780ca0e7f68de
Thegermanblob <thegermanblob@gmail.com>
2021-02-19 01:01:01 +0000

    Add rot, Append Hol

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * rot13 - translates to rot13
+ * @s: string to translate
+ * Return: translated string
+ *
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char az[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char nm[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == az[j])
+				s[i] = nm[j];
+			break;
+		}
+		i++;
+	}
+	return (s);
+}
