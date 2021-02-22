5d7c5b0a502ff69b919f75044da9267a9a5cf85b
Thegermanblob <thegermanblob@gmail.com>
2021-02-22 17:01:26 +0000

    Fix strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while(s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
+		i++;
 	}
 		return ('\0');
 }
