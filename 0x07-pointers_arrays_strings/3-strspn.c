8adba59935176186da6b3a8ee714d430f2421549
Thegermanblob <thegermanblob@gmail.com>
2021-02-23 14:19:45 +0000

    Fix strspn, no longer comma dependent

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i1, i2, rcount;
+	int i1, i2, rcount, c;
 
 	i1 = 0;
 	i2 = 0;
 	rcount = 0;
 	while (s[i1] != '\0')
 	{
+		c = 0;
 		while (accept[i2] != '\0')
 		{
 			if (accept[i2] == s[i1])
+			{
 				rcount++;
+				c++;
+			}
 			i2++;
 		}
-		if (s[i1] == ',')
+		if (c == 0)
 			break;
 		i1++;
 		i2 = 0;
