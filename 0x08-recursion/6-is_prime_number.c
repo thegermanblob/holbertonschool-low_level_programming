2623b27d4722cb4640aaaafd3147b7a62868fc97
Thegermanblob <thegermanblob@gmail.com>
2021-02-25 00:34:05 +0000

    Add prime num

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - sqr root finder
+ * @n: number
+ * @c: ne
+ * Return: sqr root
+ *
+ */
+int faccheck(int n, int c);
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n > 0)
+		return (faccheck(n, 1));
+	return (0);
+}
+/**
+ * faccheck - checks root
+ * @n:number
+ * @c:root to try
+ * Return:root
+ */
+
+int faccheck(int n, int c)
+{
+	if (n % c == 0 && c != (n / 2))
+		return (0);
+	if (c >= (c / 2))
+		return (1);
+	return (faccheck(n, c + 1));
+}
+
