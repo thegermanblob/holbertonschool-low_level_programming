e15f5e66923e22da130b26bf204e15d9b29b5931
Thegermanblob <thegermanblob@gmail.com>
2021-02-28 18:25:37 +0000

    Get rid of unused warnings

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,10 @@
  *
  *
  */
+#define UNUSED(x) (void)(x)
 int main(int argc, char *argv[])
 {
+	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,6 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+#define UNUSED(p) ( (void)(p))
 #include <stdio.h>
 
 int _putchar(char c);
