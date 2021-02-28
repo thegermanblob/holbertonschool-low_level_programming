73cc87d13a43528ee92f40622541d4e7c6197c4e
Thegermanblob <thegermanblob@gmail.com>
2021-02-28 18:06:07 +0000

    Start

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints out program name
+ * @argc: number of args to be passed
+ * @argv: strings passed
+ * Return: alway 0
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
