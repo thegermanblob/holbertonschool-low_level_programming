0371c3e9dc6644b8ecfd46ee13460e0ad7acb924
Thegermanblob <thegermanblob@gmail.com>
2021-03-02 19:22:58 +0000

    Add strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - 
+ *
+ *
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ *
+ *
+ *
+ *
+ */
+char *_strdup(char *og)
+{
+	int n, i;
+	char *cp;
+
+	n = 1 + _strlen(og);
+
+	if (n == 0)
+		return (NULL);
+	cp = malloc(n * sizeof(char));
+	if (cp == NULL)
+		return (NULL);
+	i = 0;
+	while (og[i] != '\0')
+	{
+		cp[i] = og[i];
+		i++;
+	}
+	return (cp);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+char *create_array(unsigned int n, char c);
+char *_strdup(char *og);
+
+#endif
