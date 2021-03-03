35b33d781499e4bf7d8afc842afa35a8ca8ddb69
Thegermanblob <thegermanblob@gmail.com>
2021-03-03 03:05:53 +0000

    Add 5 quick test

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - messures string
+ * @str: string to messure
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i++;
+	return (i);
+}
+/**
+ * str_concat - concanates a string up to n
+ * @s1: string 2
+ * @s2: string 1
+ * Return: concanated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i,n,j,x;
+	char *a;
+
+	for (i = 0; i < ac; i++)
+	{
+		n += _strlen(av[i]);
+	}
+	n++;
+	a = malloc(n * sizeof(char));
+	if(a == NULL)
+		return (NULL);
+	x = 0;
+
+	for(i = 1; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			a[x] = av[i][j];
+			if(j == _strlen(av[i]) - 1)
+				a[x] = '\n';
+			x++;
+		}
+	}
+	a[x] = '\0';
+	return (a);
+}
