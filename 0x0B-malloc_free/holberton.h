5ce5bda88e724e564ae0c0f975cd53c62e3f3549
Thegermanblob <thegermanblob@gmail.com>
2021-03-03 00:39:19 +0000

    Append hol

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -3,5 +3,8 @@
 
 char *create_array(unsigned int n, char c);
 char *_strdup(char *og);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
