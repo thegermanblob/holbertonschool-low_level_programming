a62ff563032f06efa1de751b3f935d647f18c146
Thegermanblob <thegermanblob@gmail.com>
2021-03-04 17:37:08 +0000

    Fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,14 +14,14 @@ int *array_range(int min, int max)
 	s = malloc(max * sizeof(int));
 	if (s == NULL)
 	{
-		free(s);
 		return (NULL);
 	}
-	for (i = 0; i < max; i++)
+	for (i = 0; i <= max; i++)
 	{
 		s[i] = min;
 		min++;
 	}
 	return ((void *)s);
 }
+
 
