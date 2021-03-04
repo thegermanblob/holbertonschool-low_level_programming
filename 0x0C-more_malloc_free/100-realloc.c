f7d9be5a01a72e5c686db6f184d85c5e3c4ba8b4
Thegermanblob <thegermanblob@gmail.com>
2021-03-04 19:20:00 +0000

    Fix check 6 and 13

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,14 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	UNUSED(old_size);
+	if (old_size == new_size)
+		return (ptr);
+
+	if(new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	free(ptr);
 	ptr = malloc(new_size);
 	return (ptr);
