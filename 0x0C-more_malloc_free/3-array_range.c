2c21265415184f876215653dff3e7ce2fbf089f0
Thegermanblob <thegermanblob@gmail.com>
2021-03-04 19:06:11 +0000

    Fix free invalid size

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,10 +12,9 @@ int *array_range(int min, int max)
 	mini = min;
 	if (min > max)
 		return (NULL);
-	s = malloc((max - min) * sizeof(int));
+	s = malloc((max - min + 1) * sizeof(int));
 	if (s == NULL)
 	{
-		free(s);
 		return (NULL);
 	}
 	for (i = 0; i <= (max - min); i++)
