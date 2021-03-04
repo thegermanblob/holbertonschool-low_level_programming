cef3e781e198c9e0d76e757bfc3b3170c861fe2f
Thegermanblob <thegermanblob@gmail.com>
2021-03-04 16:38:51 +0000

    Fix segmentation fault in 1, Make betty compliant

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@ unsigned int len(char *s)
 {
 	unsigned int i;
 
-	for(i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
 	return (i);
@@ -21,15 +21,22 @@ unsigned int len(char *s)
  * string_nconcat - joins 2 strings up to n char of s2
  * @s1: string to be coppied completly
  * @s2: string to be concanated by n chars
- * Returns: pointer to string
+ * @n: number of chars to match
+ * Return: pointer to string
  *
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, i2;
+	unsigned int i, i2, sl2;
 	char *str;
 
-	if (n >= len(s2))
+	if (s2 == NULL)
+	{
+		str = malloc((len(s1) + 1) * sizeof(char));
+		if (str == NULL)
+			return (NULL);
+	}
+	else if (n >= len(s2))
 	{
 		str = malloc((len(s1) + len(s2) + 1) * sizeof(char));
 		if (str == NULL)
@@ -50,13 +57,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (s2 != NULL)
 	{
-	while ((s2[i2] != '\0') && (i2 <= n - 1) && (n !=0))
+	while ((s2[i2] != '\0') && (i2 <= n - 1) && (n != 0))
 	{
 		str[i] = s2[i2];
 		i++;
 		i2++;
-	}
-	}
+	}}
 	str[i] = '\0';
 	return (str);
 }
