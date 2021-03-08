9bcc6aff1148ed3c9f1de35898bfdbbe8e1ba895
Thegermanblob <thegermanblob@gmail.com>
2021-03-08 17:35:21 +0000

    Make betty comp

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog
+/**
+ *  struct dog - dog
+ *  @name: dog name
+ *  @age: dog age
+ *  @owner: dog owner
+ *
+ *  Description: dog
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};/*dog*/
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
