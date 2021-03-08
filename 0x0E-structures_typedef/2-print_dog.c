8aa53793b2e9208e8ed54f874a436dab53260264
Thegermanblob <thegermanblob@gmail.com>
2021-03-08 18:07:08 +0000

    Fix check 6 and 8

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,14 +7,12 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
 	if ((*d).name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", (*d).name);
 
-	if ((*d).age == 0.0)
+	if ((*d).age <= 0)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", (*d).age);
@@ -23,6 +21,4 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", (*d).owner);
-
-	}
 }
