ce283f4f2270049e3748c61609a905ef3fe142db
Thegermanblob <thegermanblob@gmail.com>
2021-03-10 18:02:11 +0000

    Edge case NULL

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
+	if (array == NULL || size == 0 || action == NULL)
+		return;
 	for (; i < size; i++)
 	{
 		action(array[i]);
