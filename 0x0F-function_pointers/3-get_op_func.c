5262a57356a8feaebe3761808380af56962c700c
Thegermanblob <thegermanblob@gmail.com>
2021-03-10 22:19:13 +0000

    Fix check 16,18,19,23

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,6 @@ int (*get_op_func(char *s))(int, int)
 	 };
 	int i = 0;
 
-	if (s[0] != '+' && s[0] != '-' && s[0] != '*' && s[0] != '/' &&
-			s[0] != '%')
-		return (NULL);
 
 	while (*(ops[i].op) != *s && ops[i].op != NULL)
 		i++;
