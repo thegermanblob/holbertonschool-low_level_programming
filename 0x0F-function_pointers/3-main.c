6739dd2c6445345c34ddb723bcf59a660e8674a3
Thegermanblob <thegermanblob@gmail.com>
2021-03-10 22:17:10 +0000

    Fix check 16,18,19,23

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -23,8 +23,13 @@ int main(int argc, char *argv[])
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
 
-	if ((get_op_func(s) == NULL || s[1] != '\0') || (n2 == 0 && *s == '/')
-			|| (*s == '%' && n2 == 0))
+	if ((get_op_func(s) == NULL || s[1] != '\0'))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((*s == '/' && n2 == 0) || (*s == '%' && n2 == 0))
 	{
 		printf("Error\n");
 		exit(98);
