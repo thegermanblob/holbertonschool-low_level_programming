a1b1c4992fc614e7b43de1646db34c3224aa93b5
Thegermanblob <thegermanblob@gmail.com>
2021-03-10 18:05:36 +0000

    Make betty compliant

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,9 +2,10 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ * array_iterator - iterates array
+ * @array: array
+ * @size: size of array
+ * @action: function to preform
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
