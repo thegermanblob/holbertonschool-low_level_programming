0b08969faca6f3de98dcf9d2a105d4218dc48109
Thegermanblob <thegermanblob@gmail.com>
2021-03-11 16:53:46 +0000

    Make betty compliant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,22 @@
 #include "variadic_functions.h"
 /**
- *
- *
+ * sum_them_all - sum given nums
+ * @n: number of nums to sum
+ * Return: gives result
  *
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	int result;
 	unsigned int i;
-
 	va_list fargs;
-	va_start (fargs, n);
+
+	va_start(fargs, n);
 	for (i = 0; i < n; i++)
 	{
 		result += va_arg(fargs, int);
 	}
-	va_end (fargs);
+	va_end(fargs);
 
 	return (result);
 }
