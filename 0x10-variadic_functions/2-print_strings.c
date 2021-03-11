0e49c6180e134896bbf33ebe93b4261fdd6249e3
Thegermanblob <thegermanblob@gmail.com>
2021-03-11 20:11:36 +0000

    Fix all check except 6

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,14 +10,17 @@ void print_strings(const char *s, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list fargs;
+	char *str;
 
 	va_start(fargs, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(fargs, char *) != NULL)
-			printf("%s", va_arg(fargs, char *));
-		else
+		str = va_arg(fargs, char*);
+		if (str == NULL)
 			printf("(nil)");
+		else
+			printf("%s", str);
+
 		if ((i != n - 1) && (s != NULL))
 			printf("%s", s);
 	}
