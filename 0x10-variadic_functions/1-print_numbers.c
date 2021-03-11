3ce3d1afcd85e8ad10d81403aa86f2e8207407f2
Thegermanblob <thegermanblob@gmail.com>
2021-03-11 19:15:01 +0000

    Add print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints given nums seperated by given string
+ * @s: string to be used as seperate
+ * @n:number of arguments to be passed
+ *
+ */
+void print_numbers(const char *s, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list fargs;
+
+	va_start(fargs, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%i%s", va_arg(fargs, int), s);
+		else
+			printf("%i\n", va_arg(fargs, int));
+	}
+}
+
