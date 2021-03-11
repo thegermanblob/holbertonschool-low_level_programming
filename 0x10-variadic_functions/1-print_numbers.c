2ca2315ac771f3b042774e7faf4124c71871b688
Thegermanblob <thegermanblob@gmail.com>
2021-03-11 19:24:26 +0000

    Fix check 6, 10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,10 +14,13 @@ void print_numbers(const char *s, const unsigned int n, ...)
 	va_start(fargs, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1)
+		if ((i != n - 1) && (s == NULL))
+			printf("%i", va_arg(fargs, int));
+		else if (i != n - 1)
 			printf("%i%s", va_arg(fargs, int), s);
 		else
 			printf("%i\n", va_arg(fargs, int));
 	}
+	va_end(fargs);
 }
 
