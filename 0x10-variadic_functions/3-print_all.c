a53afd06a891fd9fb2cfcc07c82ea57960133a3f
Thegermanblob <thegermanblob@gmail.com>
2021-03-12 03:42:56 +0000

    Hope to god it works

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,12 +13,12 @@ void print_all(const char * const format, ...)
 	va_list fargs;
 	char *str, *coma = "";
 
-	va_start(fargs, format);
 	if (format == NULL)
 	{
 		printf("\n");
 		return;
 	}
+	va_start(fargs, format);
 	while (format[i] != '\0')
 	{
 		switch (format[i])
