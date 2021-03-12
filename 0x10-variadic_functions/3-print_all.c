dc01a1d844e5e542d1c8c2f095ca6d8606c7c872
Thegermanblob <thegermanblob@gmail.com>
2021-03-12 03:22:26 +0000

    idk i hope it work ffs

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,25 +2,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include "lol.c"
 
 /**
  *
  *
  */
-const char *_strchr(const char *s, char c)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return (s + i);
-		i++;
-	}
-	if (s[i] == c)
-		return (s + i);
-	return (NULL);
-}
 /**
  *
  *
---
+++ 0x10-variadic_functions/lol.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+const char *_strchr(const char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (s[i] == c)
+		return (s + i);
+	return (NULL);
+}
