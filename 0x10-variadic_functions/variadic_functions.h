7cb4909955081b69964c67787a621c89c0266b79
Thegermanblob <thegermanblob@gmail.com>
2021-03-12 02:37:36 +0000

    Append header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,10 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *s, const unsigned int n , ...);
+void print_all(const char * const format, ...);
+void pchar(va_list fargs);
+void pint(va_list fargs);
+void pflot(va_list fargs);
+void pstr(va_list fargs);
 
 #endif
