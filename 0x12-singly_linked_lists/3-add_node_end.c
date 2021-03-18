757a7c9455ace7b15e6853df644dbabe3318a594
Thegermanblob <thegermanblob@gmail.com>
2021-03-18 21:44:35 +0000

    Fix strlen

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - adsds node to list
+ * @head: beggining of list
+ * @str: string to be added to list
+ * Return: returns pointer to added node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, *last;
+	int i;
+
+	last = *head;
+	temp = malloc(sizeof(list_t));
+
+	if (temp == NULL)
+		return (NULL);
+
+
+	if (str == NULL)
+	{
+		temp->str = NULL;
+		temp->len = 0;
+	}
+	else
+	{
+		temp->str = strdup(str);
+		while (str[i])
+		{
+			i++;
+		}
+		temp->len = i;
+	}
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = temp;
+	return (temp);
+}
+
+
