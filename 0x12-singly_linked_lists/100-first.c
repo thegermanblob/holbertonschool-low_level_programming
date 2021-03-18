56c839c6dd2f194c4780a0ea619bcb542fb7e78e
Thegermanblob <thegermanblob@gmail.com>
2021-03-18 22:20:54 +0000

    Make betty compliant

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *
+ * premain - pre main
  *
  *
  */
 void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must %s",
+			"allow,\nI bore my house upon my back!\n");
 }
