9ea7f841f9fc4a4c20af3b696bf9c6b9dbb81140
Thegermanblob <thegermanblob@gmail.com>
2021-03-18 21:51:29 +0000

    Add free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
