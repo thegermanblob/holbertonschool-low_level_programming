9ea22a3c25156a2ea048ce07fc5859e4d8eb5728
Thegermanblob <thegermanblob@gmail.com>
2021-03-18 22:10:22 +0000

    fix logic

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -36,12 +36,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = temp;
-		return (temp);
 	}
-	while (last->next != NULL)
-		last = last->next;
-
-	last->next = temp;
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = temp;
+	}
 	return (temp);
 }
 
