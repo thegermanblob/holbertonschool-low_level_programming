5ff7a16f39ab770bb02b5ac1120bc427a5a97610
Thegermanblob <thegermanblob@gmail.com>
2021-03-30 18:09:36 +0000

    git init;Add hol; Add 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads file then print
+ * @filename: filename
+ * @letters: num of chars
+ * Return: chars writen
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int rc, file;
+	unsigned int c;
+	char data[1024];
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	rc = read(file, data, letters);
+	if (rc < 0)
+		return (0);
+
+	if (file < 0)
+		return (0);
+
+	c = write(1, data, rc);
+
+	if (c > letters)
+	{
+		return (0);
+	}
+	else
+	{
+		return (c);
+	}
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOL
+#define HOL
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
+
