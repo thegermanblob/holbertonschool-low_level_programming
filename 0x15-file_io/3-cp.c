cd31b1bcad3f9d2b7b3d6df3a8870c73f2c2cf43
Thegermanblob <thegermanblob@gmail.com>
2021-03-31 03:48:52 +0000

    Add cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -69,7 +69,7 @@ int cp_file(const char *filename, const char *newfile)
 
 
 
-	oldfile = open(filename, O_WRONLY);
+	oldfile = open(filename, O_RDWR);
 	nfile = open(newfile, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR |
 			S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	rc = read(oldfile, data, BUFSIZ);
