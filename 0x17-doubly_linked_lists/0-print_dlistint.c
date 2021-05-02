c613228e81463d560c59de067014d76b5a85d2fa
Thegermanblob <thegermanblob@gmail.com>
2021-05-02 23:40:48 +0000

    Fiz check 7

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,24 +6,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *iterator;
 	size_t i = 0;
 
-	if (h)
+	while (h)
 	{
 		printf("%i\n", h->n);
-		i++;
-	}
-	else
-		return (0);
-
-
-	if (h->next)
-		iterator = h->next;
-	while (iterator)
-	{
-		printf("%i\n", iterator->n);
-		iterator = iterator->next;
 		i++;
 	}
 	return (i);
