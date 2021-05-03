bc62e839adef0867dcb5a1c29fe3c117b1695777
Thegermanblob <thegermanblob@gmail.com>
2021-05-03 01:14:56 +0000

    Add insert

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - finds the node at the given index
+ * @head: beggining of list
+ * @index: number of link to find
+ * Return: the searched for node
+ *
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node, *new;
+	unsigned int i;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	i = 0;
+	node = *h;
+
+	while (i < idx)
+	{
+		i++;
+		node = node->next;
+	}
+	node->next->prev = new;
+	new->next = node->next;
+	node->next = new;
+	new->prev = node;
+
+	return (new);
+
+}
