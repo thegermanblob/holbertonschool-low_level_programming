79e131475909bc6eba32dbe784204ade938f3516
Thegermanblob <thegermanblob@gmail.com>
2021-05-03 01:15:06 +0000

    Add delete

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node at index
+ * @head: head
+ * @index: location of node to delete
+ * Return: -1 if fail 1 if succ
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *newn, *cur;
+	unsigned int i;
+
+	i = 0;
+	newn = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = newn->next;
+		free(newn);
+		return (1);
+	}
+	while (i != index)
+	{
+		cur = newn;
+		newn = newn->next;
+		if (newn == NULL)
+			return (-1);
+		i++;
+	}
+
+	cur->next = newn->next;
+	free(newn);
+	return (1);
+}
+
