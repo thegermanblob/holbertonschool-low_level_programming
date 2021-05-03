548b997c9a74a4e724208de737b56e74e2f76190
Thegermanblob <thegermanblob@gmail.com>
2021-05-03 01:11:31 +0000

    fix check

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,25 +1,16 @@
 #include "lists.h"
 /**
- * print_dlistint - prints list
+ * dlistint_len - prints list
  * @h:head of list
  * Return: lenght of list
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-	dlistint_t *iterator;
 	size_t i = 0;
 
-	if (h)
-		i++;
-	else
-		return (0);
-
-
-	if (h->next)
-		iterator = h->next;
-	while (iterator)
+	while (h)
 	{
-		iterator = iterator->next;
+		h = h->next;
 		i++;
 	}
 	return (i);
