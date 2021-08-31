233f5c9f07afb60ab64ae8f30ed7b15f87caf5f3
Thegermanblob <thegermanblob@gmail.com>
2021-08-31 18:49:01 +0000

    First commit

---
+++ 0x01E-search_algorithims/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * linear_search - searches given array for given value
+ * @array: given array to search
+ * @size: size of given array
+ * @value: value to search for
+ * Return: returns index of where the value is or -1 if not
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0, found = 0;
+
+	do {
+		if (array[i] == value)
+		{
+			found = 1;
+			break;
+		}
+		else
+			i++;
+	} while (i < size || found == 1);
+	if (found == 0)
+		i = -1;
+	return (i);
+}
