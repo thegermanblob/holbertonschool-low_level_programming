50ce7e7cf88b6955d395be8e8b3a86e0d8c4eca1
Thegermanblob <thegermanblob@gmail.com>
2021-09-01 02:11:27 +0000

    Add task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,6 +11,7 @@ int linear_search(int *array, size_t size, int value)
 	 unsigned int i = 0, found = 0;
 
 	do {
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			found = 1;
